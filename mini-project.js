91da1c12aba65f8c843efd25fbaa6a0eee4b4a68
Aminul islam <aminulislam424842@gmail.com>
2026-07-15 20:02:33 +0600

    feat: add personal profile console output

---
+++ mini-project.js
@@ -0,0 +1,22 @@
+let name = "Aminul";
+let age = 20;
+let country = "Bangladesh";
+let favoriteLanguage = "JavaScript";
+let job = "Full-Stack Developer";
+let yearsToGoal = 1;
+let isLearningJS = true;
+
+
+console.log("====================\n    MY PROFILE\n====================");
+console.log("");
+
+console.log("Name: "+ name);
+console.log("Age: "+ age);
+console.log("Country: "+ country);
+console.log("Favorite Language: "+favoriteLanguage);
+console.log("Dream Job: "+job);
+console.log("Years to Reach Goal: "+yearsToGoal);
+console.log("Learning JavaScript: "+isLearningJS);
+
+console.log("")
+console.log("====================")
