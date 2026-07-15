02b7d756c3f298b34b5006bbefd31b90eaa772c1
Aminul islam <aminulislam424842@gmail.com>
2026-07-15 16:33:53 +0600

    feat: implement simple math calculator

---
+++ simple-math-calculator.js
@@ -0,0 +1,15 @@
+let num1=65;
+let num2=6;
+
+add= num1+num2;
+sub = num1 - num2;
+mul = num1*num2
+div=num1 / num2;
+mod=num1 % num2;
+
+
+console.log("Addition:" + add)
+console.log("Subtraction:" + sub)
+console.log("Multiplication:" + mul)
+console.log("Division:" + div)
+console.log("Modulus:" + mod)
