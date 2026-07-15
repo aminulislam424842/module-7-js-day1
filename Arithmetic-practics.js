f5c575cae1cf0127404d934f05582fec22678218
Aminul islam <aminulislam424842@gmail.com>
2026-07-15 19:24:52 +0600

    feat: implement basic arithmetic operations in JavaScript

---
+++ Arithmetic-practics.js
@@ -0,0 +1,16 @@
+let a=50;
+let b=8;
+
+add = a+b;
+sub = a-b;
+mul = a * b;
+div = a/b;
+mod = a%b;
+
+console.log("Number 1 = "+ a)
+console.log("Number 2 = "+ b)
+console.log("Addition:" + add);
+console.log("Subtraction:" + sub);
+console.log("Multiplication:" + mul)
+console.log("Division:" + div)
+console.log("Modulus:" + mod);
