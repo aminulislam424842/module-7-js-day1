4ab2b12420bdb38458bb781aff01146aaa5f0802
Aminul islam <aminulislam424842@gmail.com>
2026-07-15 20:41:05 +0600

    feat: practice JavaScript data type conversions

---
+++ data-convertion.js
@@ -0,0 +1,118 @@
+// 1
+let num = "50";
+let newNum = Number(num);
+
+console.log(num);
+console.log(typeof newNum);
+console.log();
+
+// 2
+let age =20;
+let newAge = String(age);
+
+console.log(age);
+console.log(typeof newAge);
+console.log();
+
+// 3
+let value =1;
+let newValue = Boolean(value);
+
+console.log(value);
+console.log(newValue);
+console.log();
+
+// 4
+let price = "450px";
+let newPrice = parseInt(price);
+
+console.log(newPrice);
+console.log(typeof newPrice);
+console.log();
+
+// 5
+let weight = "72.8kg";
+let newWeight = parseFloat(weight);
+
+console.log(newWeight);
+console.log(typeof newWeight);
+console.log();
+
+// 6
+let a = "100";
+let newA = Number(a);
+
+console.log(newA);
+console.log(typeof newA);
+console.log();
+
+// 7
+let b = "200.5";
+let newB = Number(b);
+
+console.log(newB);
+console.log(typeof newB);
+console.log();
+
+// 8
+let c = "";
+let newC = Boolean(c);
+
+console.log(newC);
+console.log();
+
+// 9
+let d = "Hello";
+let newD = Boolean(d);
+
+console.log(d);
+console.log(newD);
+console.log();
+
+// 10
+let e = "50px";
+let newE = parseInt(e);
+
+console.log(newE);
+console.log(typeof newE);
+console.log();
+
+// 11
+let f = "25.75kg";
+let newF = parseFloat(f);
+
+console.log(newF);
+console.log(typeof newF);
+console.log();
+
+// 12
+let g = 0;
+let newG = String(g);
+
+console.log(newG);
+console.log(typeof newG);
+console.log();
+
+// 13
+let h = 1;
+let newH = String(h);
+
+console.log(newH);
+console.log(typeof newH);
+console.log();
+
+// 14
+let i = null;
+let newI = String(i);
+
+console.log(newI);
+console.log(typeof newI);
+console.log();
+
+// 15
+let j = undefined;
+
+let newJ = Number(j);
+
+console.log(newJ);
+console.log(typeof newJ);
