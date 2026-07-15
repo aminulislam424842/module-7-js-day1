2239e2ef8bd486ef15729b0856b2549fdeff79e7
Aminul islam <aminulislam424842@gmail.com>
2026-07-15 19:17:31 +0600

    feat: add personal introduction using JavaScript variables

---
+++ personal-bio.js
@@ -0,0 +1,12 @@
+let name ="aminul";
+let age = 20;
+let favoriteFood = "burger";
+let favoriteLanguage = "JavaScript";
+let lovesCoding = true;
+
+console.log("Hello!");
+console.log("My name is " + name);
+console.log("I am "+ age + " years old");
+console.log("My favorite food is " + favoriteFood);
+console.log("My favorite language is " + favoriteLanguage);
+console.log("I love coding:" + lovesCoding);
