1f088ebba7a6a5fa60f2cf7df4a3fa0497ac06fe
Aminul islam <aminulislam424842@gmail.com>
2026-07-15 19:42:54 +0600

    feat: add mini coding practice solutions

---
+++ mini-coding.js
@@ -0,0 +1,11 @@
+let price = 200;
+let quantity = 3;
+let discount = 10;
+
+totalPrice = quantity * price ;
+discountAmount = totalPrice / discount;
+finalPrice = totalPrice - discountAmount;
+
+console.log("Total Price: " + totalPrice)
+console.log("Discount Amount: " + discountAmount)
+console.log("Final Price: " + finalPrice)
